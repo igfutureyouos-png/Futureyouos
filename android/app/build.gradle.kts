b69ca8c69cca581018f06a0f55a285745b600ca2
mtben82-coder <mtben82@gmail.com>
2025-12-16 01:53:57 +0000

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 500
-        versionName = "500"
+        versionCode = 501
+        versionName = "501"
         multiDexEnabled = true
     }
 
